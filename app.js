bc12884b698d5e74ee7a63abb3c65320a094f393
SANS-1104 <sanskriti020803@gmail.com>
2024-05-27 16:08:11 +0530

    new changes

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 8800;
+const port = 8080;
 const path = require("path");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
-
+const ejsMate = require("ejs-mate");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static (path.join(__dirname,"public")));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+
 
 main()
 .then(() => {
@@ -28,8 +30,9 @@ app.listen(port, () =>{
     console.log(`App is Listening to port no ${port}`);
 });
 
-app.get("/", (req,res) =>{
-    res.send("It's Working Fine!!");
+app.get("/", async(req,res) =>{
+    const allListings = await Listing.find();
+    res.render("listings/index.ejs",{allListings});
 });
 
 // app.get("/test",async (req,res)=>{
@@ -49,17 +52,65 @@ app.get("/", (req,res) =>{
 
 app.get("/listings",async (req,res) =>{
     const allListings = await Listing.find();
-   res.render("index.ejs",{allListings}); 
+   res.render("listings/index.ejs",{allListings}); 
 });
 
 // Show route
 
 app.get("/listings/:id", async(req,res)=>{
-    let {id} = req.params;
-    const x = await Listing.findById(id);
-    res.render("show.ejs", {x});
+    try{
+
+        let {id} = req.params;
+        const x = await Listing.findById(id);
+        console.log(x)
+        if (x){
+            res.render("listings/show.ejs", {x});
+        }
+        else{
+            res.render("listings/error.ejs")
+        }
+    }
+    catch(err){
+        res.render("listings/error.ejs")
+    }
 });
 
-app.get("/listings/new",(req,res)=>{
-    res.render("new.ejs");
-})
+
+//Getting to the creating new listing page
+
+app.get("/listings-new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+
+//submiting the listing after creating it
+
+app.post("/listings" , async(req,res) =>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//Getting to the editing listing page
+
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    const x = await Listing.findById(id);
+    res.render("listings/edit.ejs",{x});
+});
+
+//submiting the listing after editing it
+
+app.put("/listings/:id",async (req,res) =>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete an existing listing
+
+app.delete("/listings/:id", async (req,res) =>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
